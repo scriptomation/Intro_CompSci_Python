37a56f080b24ea35f691b438ca1e81977f7a1d9b
Tommy Stone <tommystone@gmail.com>
2017-11-18 15:31:30 -0800

    Code complete. It will take a variable from s and search through to see which is the longest section that is in alphabetical order.

--- Week_1/Problem Set 3.py
+++ Week_1/Problem Set 3.py
@@ -6,12 +6,14 @@ This is a temporary script file.
 """
 
 #s = 'azcbobobegghakl'
-s = 'azcbobobegghakl'
+s = 'abcbcd'
 
 #alphabetical order
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 #temporary storage of alphabet
+sindex = 0
 lindex = 0
+tmpletter = ''
 tmporder = ''
 #variable used to store the string with the longest alphabetical order
 laorder = ''
@@ -22,21 +24,28 @@ for letter in s:
     #function to find where the letter is in the alphabet string
     for item in alphabet:
         if item == letter: #or (aindex >= len(alphabet) - 1):
-            print("s letter is: " + letter)
-            print("Alphabet letter is: " + item)
+            #print("s letter is: " + letter)
+            #print("Alphabet letter is: " + item)
             break
         aindex += 1
-
-    #function  
+    #goes through each letter in string starting from index and compares to the
+    #last
+    tmpletter = letter
+    for item2 in s[sindex:]:
+        if tmpletter <= item2:
+            tmpletter = item2
+            tmporder += tmpletter
+        else:
+            break
+    if len(tmporder) > len(laorder):
+        laorder = tmporder
+    #reset alphabet index   
     aindex = 0
+    #Increment string index
+    sindex += 1
+    tmporder = ''
+print('Longest substring in alphabetical order is: ' + laorder)
 
-
-#reset variables
-tmpindex = 0
-tmporder = ''
-laorder = ''
-count = 0
-index = 0   
 
 
     #function will test to see if the next letter is the same as the letter in
