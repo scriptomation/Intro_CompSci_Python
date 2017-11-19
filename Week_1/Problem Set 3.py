b6387f53ce2974de74846f60fa29da131a695d9d
Tommy Stone <tommystone@gmail.com>
2017-11-18 18:38:23 -0800

    removed all tests from program

--- Week_1/Problem Set 3.py
+++ Week_1/Problem Set 3.py
@@ -12,7 +12,6 @@ s = 'abcbcd'
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 #temporary storage of alphabet
 sindex = 0
-lindex = 0
 tmpletter = ''
 tmporder = ''
 #variable used to store the string with the longest alphabetical order
@@ -24,8 +23,6 @@ for letter in s:
     #function to find where the letter is in the alphabet string
     for item in alphabet:
         if item == letter: #or (aindex >= len(alphabet) - 1):
-            #print("s letter is: " + letter)
-            #print("Alphabet letter is: " + item)
             break
         aindex += 1
     #goes through each letter in string starting from index and compares to the
@@ -45,17 +42,3 @@ for letter in s:
     sindex += 1
     tmporder = ''
 print('Longest substring in alphabetical order is: ' + laorder)
-
-
-
-    #function will test to see if the next letter is the same as the letter in
-    #in the alphabet.  If it is it will add it to the tmporder variable
-    #If not it will continue the function
-#    for aletter in alphabet[aindex:]:
-#        if aletter == letter or aletter == alphabet[aindex]:
-#            count += 1
-#            tmporder += aletter
-#        else:
-#            break
-#        if len(tmporder) > len(laorder):
-#            laorder = tmporder
